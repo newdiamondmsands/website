d067fd1fac08ce8465bc2676fd8c3d132edf5209
muhammed ramshin <muhammedramshin@gmail.com>
2020-09-24 09:34:02 +0530

    initial commit

---
+++ header.php
@@ -0,0 +1,79 @@
+<head>
+    <title>New Diamond &mdash; Investment For Life Time</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+  
+  <div class="site-wrap"  id="home-section">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+   
+      
+    <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
+
+      <div class="container">
+        <div class="row align-items-center position-relative">
+          
+            
+            <div class="site-logo">
+              <img src="images/logo.jpg" alt="logo" width="140px" height="60px" style="float:left">
+            </div>
+            
+            <div class="col-12">
+              <nav class="site-navigation text-center " role="navigation">
+
+                <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
+                  <li><a href="#home-section" class="nav-link"> </a></li>
+                  <li><a href="#home-section" class="nav-link"> </a></li>
+                  <li><a href="#home-section" class="nav-link"> </a></li>
+                <li><a href="#home-section" class="nav-link">Home</a></li>
+                <li><a href="press-single.html" class="nav-link">New Diamond Group</a></li>
+                <li><a href="#about-section" class="nav-link">Our Products</a></li>
+                
+                
+                
+                <li><a href="#team-section" class="nav-link">Team</a></li>
+                <li><a href="#contact-section" class="nav-link">Contact Us</a></li>
+              </ul>
+              </nav>
+          
+            </div>
+
+          
+            <div class="toggle-button align-items-center d-flex">
+              <a href="#" class="btn btn-primary bg-blue" data-toggle="modal" data-target="#exampleModal">Get Quote</a>
+              <a href="#" class="site-menu-toggle p-1 js-menu-toggle text-black d-inline-block d-lg-none d-flex"><span class="icon-menu h3 m-0"></span></a>
+            </div>
+
+        </div>
+      </div>
+      
+    </header>
+    
